534bb02a27484fc6bf78f94a395431836702e2a0
Lina Vo <llina.tvo@gmail.com>
2021-10-06 18:45:03 +0200

    Create Header component and add css

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Movie search app</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
